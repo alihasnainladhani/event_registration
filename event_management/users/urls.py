106046ca722e5b2d8e4743416279a1df2a16c194
shahzaib2028 <mohammadshahzaib2028@gmail.com>
2025-06-19 15:39:32 +0500

    update code

--- event_management/users/urls.py
+++ event_management/users/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 
 from event_management.users.views import (
     user_detail_view,
-    user_redirect,
+    user_redirect_view,
     user_update_view,
 )
 
 app_name = "users"
 urlpatterns = [
-    path("~redirect/", view=user_redirect, name="redirect"),
+    path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
 ]
